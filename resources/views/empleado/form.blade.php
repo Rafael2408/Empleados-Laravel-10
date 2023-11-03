f90ed44a4b8f704349708ca5c0e4f57bb77de8ad
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2023-11-03 17:56:47 -0500

    upload all project

---
+++ resources/views/empleado/form.blade.php
@@ -0,0 +1,48 @@
+
+<h1>{{ $modo }} Empleado</h1>
+
+@if(count($errors)>0)
+<div class="alert alert-danger" role="alert">
+        <ul>
+            
+            @foreach($errors->all() as $error)
+            <li> {{ $error }} </li>
+            @endforeach
+        </ul>
+    </div>
+
+@endif
+
+<div class="form-group">
+
+<label for="Nombre">Nombre</label>
+<input type="text" class="form-control" name="Nombre" 
+value="{{isset($empleado->Nombre)? $empleado->Nombre : old('Nombre') }}" id="Nombre">
+
+
+<label for="ApellidoPaterno">ApellidoPaterno</label>
+<input type="text" class="form-control" name="ApellidoPaterno" 
+value="{{isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:old('ApellidoPaterno') }}" id="ApellidoPaterno">
+
+
+
+<label for="ApellidoMaterno">ApellidoMaterno</label>
+<input type="text" class="form-control" name="ApellidoMaterno" 
+value="{{isset($empleado->ApellidoMaterno)? $empleado->ApellidoMaterno:old('ApellidoMaterno')}}" id="ApellidoMaterno">
+
+<label for="Correo">Correo</label>
+<input type="email" class="form-control" name="Correo" 
+value="{{isset($empleado->Correo) ? $empleado->Correo: old('Correo')}}" id="Correo">
+
+<label for="Foto"></label>
+@if(isset($empleado->Foto))
+<img class="img-thumnail img-fluid" src="{{ asset('storage').'/'.$empleado->Foto }}" width="100px" alt="">
+@endif
+<input type="file" class="form-control" name="Foto" value="" id="Foto">
+
+</div>
+
+<br>
+<input class="btn btn-success" type="submit" value="{{$modo}} Datos" >
+<a class="btn btn-primary" href="{{ url('empleado/') }}">Regresar </a>
+<br>
